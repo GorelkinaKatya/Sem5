2ddd0a7e92d83876ae95f33d565118b9909a077f
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-06 19:18:11 +0300

    done task 34

---
+++ Task34/Program.cs
@@ -0,0 +1,24 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(99, 1000);
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element % 2 == 0)
+            count++;       
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четных чисел в массиве: {CountEvenNumbers(array)}");
